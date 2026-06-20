c1ec8251a6137591baecb6ad8f09d7e7fe796df3
sukhmaygiri <sukhmaygiri7@gmail.com>
2026-06-20 22:38:54 +0530

    Create security.tf

---
+++ terraform/security.tf
@@ -0,0 +1,29 @@
+resource "aws_security_group" "ec2_sg" {
+
+  name        = "ec2-sg"
+  description = "Allow SSH"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "SSH"
+
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ec2-sg"
+  }
+}
